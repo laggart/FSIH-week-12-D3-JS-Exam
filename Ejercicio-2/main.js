c025d779ca480296e4d17ca993da3eb85edc473a
Juan Carlos Pineda <laggart@gmail.com>
2024-04-04 21:29:17 +0200

    ejercicio 2 en función

--- Ejercicio-2/main.js
+++ Ejercicio-2/main.js
@@ -1,11 +1,21 @@
 //////////////////////////////////////////////////////////////////////////////////////////
 
 // Ejercicio 2
+function mascotaYNombre(tipoMascota, nombreMascota){
+    console.log(`tu mascota es un ${tipoMascota} y su nombre es ${nombreMascota}`)
+}
+    
 
-const tipoMascota = prompt('Que tipo de mascota tienes?')
-const nombreMascota = prompt('cual es el nombre de tu mascota?')
+    const tipoMascota = prompt('Que tipo de mascota tienes?')
+    const nombreMascota = prompt('cual es el nombre de tu mascota?')
 
-console.log(`tu mascota es un ${tipoMascota} y su nombre es ${nombreMascota}`)
+    mascotaYNombre(tipoMascota,nombreMascota)
+    
+
+
+
+
+
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
